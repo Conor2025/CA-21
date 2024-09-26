8a921db4850cb94d84a87af02efe9ad67115185b
Conor2025 <146126867+Conor2025@users.noreply.github.com>
2024-09-26 11:31:49 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+st.title("Interactive Sales Dashboard")
+st.write("This dashboard allows you to view sales data by year")
+
+data = {
+  'Year': [2010, 2011, 2012, 2013, 2014, 2015],
+  'Region': ['North', 'South', 'East', 'West', 'Central', 'North'],
+  'Sales Amount': [200, 210, 220, 230, 240, 250]
+}
+df = pd.DataFrame(data)
+
+selected_year = st.slider('Select a year', min_value = 2010, max_value = 2015, value = 2013)
+
+st.write(f"Date for the year {selected_year}:")
+filtered_df = df[df['Year'] == selected_year]
+st.write(filtered_df)
+
+flg, ax = plt.subplots()
+ax.bar(filtered_df['Region'], filtered_df['Sales Amount'], color = 'skyblue')
+plt.title(f'Sales in {selected_year},)
+plt.xlabel('Region')
+plt.ylabel('Sales Amount')
+st.pyplot(fig)
